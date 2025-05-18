894454fd6d999505730ee762921fcb618ff7573c
Shahbaz Khan <khanshahbaz1773@gmail.com>
2025-05-18 16:46:02 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st 
+import joblib
+
+vectorizer = joblib.load("Vectorizer.jb")
+model = joblib.load("gbc_model.jb")
+
+st.title("Fake News Detector")
+st.write("Enter a News article below : ")
+
+news_input = st.text_area("Enter the News Article: ")
+
+if st.button("Check News"):
+    if news_input.strip():
+        transform_input = vectorizer.transform([news_input])
+        prediction = model.predict(transform_input)
+
+        if prediction[0] == 1:
+            st.success("The News is real")
+        else:
+            st.error("The News is fake")
+    else:
+        st.warning("Please enter some text to analyze")
